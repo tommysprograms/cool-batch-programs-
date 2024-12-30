8d14bb44287c87b3805a019408a4484c84922419
tommysprograms <tommypane2011@gmail.com>
2024-12-30 14:52:06 +0100

    Update Animatey.bat

--- Animatey.bat
+++ Animatey.bat
@@ -3,86 +3,84 @@ color 0E
 :loop
 
 echo Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo  Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo   Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo    Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo     Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo      Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo       Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo        Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo         Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo          Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo           Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo            Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo             Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo              Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo               Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                 Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                  Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                   Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                    Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                     Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                      Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                       Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                        Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                         Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                          Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
 echo                           Animatey by Tommy - 27-10-24
-timeout 0
+timeout 0 >nul
 cls
-echo                            Animatey by Tommy - 27-10-24
-timeout 0
 goto loop
