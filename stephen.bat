4c12060997bc7486dc63cf3516580188971f615e
tommysprograms <tommypane2011@gmail.com>
2024-12-30 14:54:04 +0100

    Update stephen.bat

--- stephen.bat
+++ stephen.bat
@@ -1,15 +1,16 @@
 @echo off
+@MODE CON COLS=54 LINES=30
 color 0E
-
+title saint stephen's day
 echo sorry grandma, i've already ate too much for St Stephen's Day...
-timeout 3
+timeout 3 >nul
 cls
 echo i would love to eat more but i just can't...
-timeout 3
+timeout 3 >nul
 cls
 echo i still love your food tho :)
-timeout 3
+timeout 3 >nul
 cls
 echo STEPHEN VIRUS / 26-12-2024 / MADE BY TOMMY
-timeout 5
+timeout 5 >nul
 cls
