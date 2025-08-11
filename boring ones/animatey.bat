0da1155770fdcea4d6e263d6f9e5aa70cfe43cb5
tommy <tommypane2011@gmail.com>
2025-08-11 19:07:42 +0200

    Create animatey.bat

---
+++ boring ones/animatey.bat
@@ -0,0 +1,87 @@
+@echo off
+@MODE CON COLS=60 LINES=30
+color 0E
+:loop
+title animatey
+echo Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo  Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo   Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo    Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo     Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo      Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo       Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo        Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo         Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo          Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo           Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo            Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo             Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo              Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo               Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                 Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                  Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                   Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                    Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                     Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                      Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                       Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                        Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                         Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                          Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+echo                           Animatey by Tommy - 27-10-24
+timeout 0 >nul
+cls
+goto loop
