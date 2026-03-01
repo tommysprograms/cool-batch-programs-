82a146c2cc04e6ea76988233168bbf389c2be778
tommy <tommypane2011@gmail.com>
2026-03-01 16:52:44 +0100

    PETYA FINALLY WORKS!!!! THANKS COPILOT
    this took so fucking long... jfc

--- petya.bat
+++ petya.bat
@@ -2,11 +2,7 @@
 CHCP 65001 >nul
 setlocal ENABLEDELAYEDEXPANSION
 title petya
-mode con cols=80 lines=25
 
-:: ======================
-:: 1. Fake CHKDSK Stage
-:: ======================
 for /L %%p in (0,1,100) do (
     cls
     echo Repairing file system on C:
@@ -20,64 +16,79 @@ for /L %%p in (0,1,100) do (
     echo DESTROY ALL OF YOUR DATA^^! PLEASE ENSURE THAT YOUR POWER CABLE IS PLUGGED
     echo IN^^!
     echo.
-    echo CHKDSK is repairing sectors... %%p%% complete.
-    
-    :: Random delay (1–2 sec)
+    echo CHKDSK is repairing sectors... %%p%% complete. 
+
     set /a delay=!random! %% 2 + 1
     timeout /t !delay! >nul
 )
 
-:: ======================
-:: 2. Skull Payload
-:: ======================
 :skull_loop
-color 4F
-call :skull
-timeout /t 1 >nul
-color F4
-call :skull
-timeout /t 1 >nul
-
-:: check for keypress
-choice /c ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 /n /t 0 /d Z >nul
-if errorlevel 1 goto ransom
-goto skull_loop
-
-:skull
-cls
-echo                     uu$$$$$$$$$$$uu
-echo                   uu$$$$$$$$$$$$$$$$$uu
-echo                  u$$$$$$$$$$$$$$$$$$$$$u
-echo                  u$$$$$$$$$$$$$$$$$$$$$u
-echo                u$$$$$$$$$$$$$$$$$$$$$$$$$u
-echo                u$$$$$$$$$$$$$$$$$$$$$$$$$u
-echo                u$$$$$$*   *$$$*   *$$$$$$u
-echo                *$$$$*      u$u       $$$$*
-echo                 $$$u       u$u       u$$$
-echo                 $$$u      u$$$u      u$$$
-echo                  *$$$$uu$$$   $$$uu$$$$*
-echo                   *$$$$$$$*   *$$$$$$$*
-echo                     u$$$$$$$u$$$$$$$u
-echo                      u$*$*$*$*$*$*$u
-echo           uuu        $$u$ $ $ $ $u$$       uuu 
-echo          u$$$$        $$$$$u$u$u$$$       u$$$$
-echo           $$$$$uu      *$$$$$$$$$*     uu$$$$$$
-echo         u$$$$$$$$$$$uu    *****    uuuu$$$$$$$$$
-echo         $$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$*
-echo          ***      **$$$$$$$$$$$uu **$***
-echo                   uuuu **$$$$$$$$$$uuu
-echo          u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$
-echo          $$$$$$$$$$****           **$$$$$$$$$$$*
-echo            *$$$$$*                      **$$$$**
-echo              $$$*     PRESS ANY KEY^^!    $$$$*
-exit /b
+(
+echo $skull = @'
+echo.
+echo.
+echo                                                   uu$$$$$$$$$$$uu
+echo                                                 uu$$$$$$$$$$$$$$$$$uu
+echo                                                u$$$$$$$$$$$$$$$$$$$$$u
+echo                                                u$$$$$$$$$$$$$$$$$$$$$u
+echo                                              u$$$$$$$$$$$$$$$$$$$$$$$$$u
+echo                                              u$$$$$$$$$$$$$$$$$$$$$$$$$u
+echo                                              u$$$$$$*   *$$$*   *$$$$$$u
+echo                                              *$$$$*      u$u       $$$$*
+echo                                               $$$u       u$u       u$$$
+echo                                               $$$u      u$$$u      u$$$
+echo                                                *$$$$uu$$$   $$$uu$$$$*
+echo                                                 *$$$$$$$*   *$$$$$$$*
+echo                                                   u$$$$$$$u$$$$$$$u
+echo                                                    u$*$*$*$*$*$*$u
+echo                                         uuu        $$u$ $ $ $ $u$$       uuu 
+echo                                        u$$$$        $$$$$u$u$u$$$       u$$$$
+echo                                         $$$$$uu      *$$$$$$$$$*     uu$$$$$$
+echo                                       u$$$$$$$$$$$uu    *****    uuuu$$$$$$$$$
+echo                                       $$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$*
+echo                                       ***      **$$$$$$$$$$$uu **$***
+echo                                                 uuuu **$$$$$$$$$$uuu
+echo                                        u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$
+echo                                        $$$$$$$$$$****           **$$$$$$$$$$$*
+echo                                          *$$$$$*                      **$$$$**
+echo                                            $$$*     PRESS ANY KEY      $$$$*
+echo '@
+echo.
+echo while^($true^) {
+echo     [Console]::ForegroundColor = 'White'
+echo     [Console]::BackgroundColor = 'DarkRed'
+echo     cls
+echo     Write-Host $skull
+echo     for^($i = 0; $i -lt 10; $i++^) {
+echo         if ^([Console]::KeyAvailable^) {
+echo             [Console]::ReadKey^($true^) ^| Out-Null
+echo             exit
+echo         }
+echo         [System.Threading.Thread]::Sleep^(10^)
+echo     }
+echo     [Console]::ForegroundColor = 'DarkRed'
+echo     [Console]::BackgroundColor = 'White'
+echo     cls
+echo     Write-Host $skull
+echo     for^($i = 0; $i -lt 10; $i++^) {
+echo         if ^([Console]::KeyAvailable^) {
+echo             [Console]::ReadKey^($true^) ^| Out-Null
+echo             exit
+echo         }
+echo         [System.Threading.Thread]::Sleep^(10^)
+echo     }
+echo }
+) > skull.ps1
+powershell -ExecutionPolicy Bypass -File skull.ps1
+del skull.ps1 >nul 2>&1
+goto ransom
 
 :ransom
 cls
 color 4F
 echo You became victim of the PETYA RANSOMWARE^^!
 echo.
-echo █████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+echo ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 echo.
 echo The hard disks of your computer have been encrypted with a military grade
 echo encryption algorithm. There is no way to restore your files without a special
@@ -99,17 +110,19 @@ echo.
 echo If you already purchased your key, please enter it below.
 echo.
 
-set /p "Key=Enter key: " >nul
+set /p "Key=Key: " 
 
-if "%Key%"=="testkey" goto decrypt
+if "%Key%"=="subscribe" goto decrypt
 goto error
 
 :decrypt
-echo x=msgbox("File decryption successful!",0,"Decryption attempt...") > decryption.vbs
-start decryption.vbs
+echo x=msgbox("File decryption successful!",0,"Decryption attempt...") > "%cd%\decryption.vbs"
+cscript.exe //nologo "%cd%\decryption.vbs"
+del "%cd%\decryption.vbs"
 goto :eof
 
 :error
-echo x=msgbox("Incorrect key!",0,"Error 404") > error.vbs
-start error.vbs
-
+echo x=msgbox("Incorrect key^!",0,"Error 404") > "%cd%\error.vbs"
+cscript.exe //nologo "%cd%\error.vbs"
+del "%cd%\error.vbs"
+goto ransom
